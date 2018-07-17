7c97557f43c8f24c0a6920911ba80dab3ce5b0d6
kipsangtitus95@gmail.com <kipsangtitus95@gmail.com>
2018-07-17 16:36:01 +0300

    The passenger now able to enter details

---
+++ ConsoleApp7/Flight.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    public class Flight
+    {
+        public static string Name { get; set; }
+        public static int Passportnumber { get; set; }
+        public static DateTime DateTime { get; set; }
+        public static string Destination { get; set; }
+        public static string Residence { get; set; }
+        public static string Category { get; set; }
+        public static int Amount { get; set; }
+       
+        
+        public static void Input()
+        {
+            Console.WriteLine("Enter Name");
+            Name = Console.ReadLine();
+            Console.WriteLine("Enter Passport number");
+            Passportnumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Residence");
+            Residence = Console.ReadLine();
+            Console.WriteLine("Enter destination");
+            Destination = Console.ReadLine();
+            Console.WriteLine("Enter category");
+            Category = Console.ReadLine();
+        }
+
+        public static void display()
+        {
+            Console.WriteLine($"name: {Name}");
+            Console.WriteLine($"Passport number: {Passportnumber}");
+            Console.WriteLine($"Residence: {Residence}");
+            Console.WriteLine($"Destination: {Destination}");
+        }
+         
+        public static void BookingReturnFlight()
+        {
+            
+           
+        }
+    }
+}
---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Flight Titus=new Flight();
+            Flight.Input();
+            Console.Clear();
+            Flight.display();
+            
+            Console.ReadKey();
+        }
+    }
+}
